ee2da5f273ccff000ee68e7cb0b066906980ec1c
N1-San <33519431+N1-San@users.noreply.github.com>
2025-05-16 17:37:13 +0530

    making unused things comment

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 
 import 'package:expense_tracker/models/expense.dart';
 
